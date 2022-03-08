879e9d7aaf60075920bf9350da3dbf3e184d9c34
Luidenka-Golden <87653308+Luidenka-Golden@users.noreply.github.com>
2022-03-08 08:39:08 +0800

    Bug-fix
    Fixed the bug that highlight yesterday's date

--- calendar.js
+++ calendar.js
@@ -1,9 +1,30 @@
-function setCalendar(table)
+function setCalendar()
 {
 var d = new Date;
 var y = d.getFullYear();
 var m = d.getMonth() + 1
 let w = new Date(y + "-" + m + "-01").getDay();
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ["", "January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"];
+var table = document.createElement('table');
+table.setAttribute('id', 't');
+table.style.width = '100%';
+document.getElementById('calendar').appendChild(table);
+var table = document.createElement('tbody');
+table.setAttribute('id', 'month');
+document.getElementById('t').appendChild(table);
+var week = document.createElement('tr');
+week.setAttribute('id', 'week')
+document.getElementById('month').appendChild(week)
+for (let i = 0; i < weekDays.length; i++) {
+    var td = document.createElement('td');
+    if (weekDays[i] == 'Sunday') {
+        td.style.color = 'red';
+    }
+    td.innerHTML = weekDays[i];
+    document.getElementById('week').appendChild(td);
+}
 var tr = document.createElement('tr');
 tr.setAttribute('id', 'tr1')
 table.appendChild(tr);
@@ -35,6 +56,7 @@ else {
 }
 function setTable() {
     numberOfDays = new Date(y, m, 0).getDate();
+    console.log(d.getDate())
     for (let i = 1; i < numberOfDays + 1; i++) {
         function checkToday() {
             if (i == d.getDate()) {
@@ -45,37 +67,37 @@ function setTable() {
             if (table.rows[2].cells.length == 7) {
                 if (table.rows[3].cells.length == 7) {
                     if (table.rows[4].cells.length == 7) {
-                        checkToday()
                         var t = document.createElement('td');
                         t.innerHTML = i.toString()
                         document.getElementById('tr5').appendChild(t);
+                        checkToday()
                     }
                     else {
-                        checkToday()
                         var t = document.createElement('td');
                         t.innerHTML = i.toString()
                         document.getElementById('tr4').appendChild(t);
+                        checkToday()
                     }
                 }
                 else {
-                    checkToday()
                     var t = document.createElement('td');
                     t.innerHTML = i.toString()
                     document.getElementById('tr3').appendChild(t);
+                    checkToday()
                 }
             }
             else {
-                checkToday()
                 var t = document.createElement('td');
                 t.innerHTML = i.toString()
                 document.getElementById('tr2').appendChild(t);
+                checkToday()
             }
         }
         else {
-            checkToday()
             var t = document.createElement('td');
             t.innerHTML = i.toString()
             document.getElementById('tr1').appendChild(t);
+            checkToday()
         }
     }
 }}
